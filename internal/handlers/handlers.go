1115164c7cc3a7a94e2875723bd8f93fceeb4bdf
Almaz Mustafin <drugdruk@gmail.com>
2026-06-02 23:14:11 +0500

    fix correct import path

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,7 +4,7 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"://github.com"
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
 )
 
 func ServeIndexHandler(w http.ResponseWriter, r *http.Request) {
